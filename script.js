95c468e8f21efe7f283ca379c6f1ff2fc6a5bbad
yuripujol <yuripujolcorrea@gmail.com>
2024-05-11 16:01:34 -0300

    if e else adicionado

--- script.js
+++ script.js
@@ -6,10 +6,24 @@ function sortearNúmero (){
     const númeromenor =  Math.ceil (document. querySelector (".número-menor").value)
     const númeromaior = Math.floor (document.querySelector(".número-maior").value)
 
-   
-    const resultado = Math.floor(Math.random() * (númeromaior - númeromenor) + númeromenor);
+  
 
-alert(resultado)
+
+if (númeromenor >= númeromaior){
+
+
+alert("o valor minimo precisa ser menor que o valor máximo")
+
+}
+
+else{
+
+   
+    const resultado = Math.floor(Math.random() * ( númeromaior - númeromenor + 1)) + númeromenor;
+    alert(resultado)
+}
+
+
 }
 
 
